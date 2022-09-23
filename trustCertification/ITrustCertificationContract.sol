2adbe7bcc803b35878043b7bf5e3421dedc95c46
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2022-09-23 18:45:40 +0200

    Refactoring issue certificate method

--- trustCertification/ITrustCertificationContract.sol
+++ trustCertification/ITrustCertificationContract.sol
@@ -4,7 +4,7 @@ pragma experimental ABIEncoderV2;
 
 interface ITrustCertificationContract { 
     
-    function issueCertificate(address _recipientAddress, string memory _certificateCourseId, uint _qualification, string memory _cid, string memory _certificateHash) external returns(string memory);
+    function issueCertificate(IssueCertificateRequest memory _request) external returns(string memory);
     function renewCertificate(string memory _id) external;
     function enableCertificate(string memory _id) external;
     function disableCertificate(string memory _id) external;
@@ -13,23 +13,37 @@ interface ITrustCertificationContract {
     function getCertificateDetail(string memory _id) external view returns (CertificateRecord memory);
     function getMyCertificatesAsRecipient() external view returns (string[] memory);
     function getMyCertificatesAsIssuer() external view returns (string[] memory);
-    function validateCertificateIntegrity(string  memory _certificateHash) external view returns (bool);
+    function validateCertificateIntegrity(string memory _id, string  memory _fileCertificateHash, address recipientAddress) external view returns (bool);
     
     // Data Structure
     struct CertificateRecord {
+        string id;
         address issuerAddress;
         address recipientAddress;
         string course;
         uint256 expirationDate;
         uint qualification;
         uint durationInHours;
-        string cid;
-        string certificateHash;
+        string fileCid;
+        string fileCertificateHash;
+        string imageCid;
+        string imageCertificateHash;
         uint256 expeditionDate;
         bool isVisible;
         bool isEnabled;
         bool isExist;
     } 
+
+    struct IssueCertificateRequest {
+        string id;
+        address recipientAddress;
+        string certificateCourseId; 
+        uint qualification;
+        string fileCid;
+        string fileCertificateHash;
+        string imageCid; 
+        string imageCertificateHash;
+    }
 
     // Events
     event OnNewCertificateGenerated(string _id, bool _isVisible);
