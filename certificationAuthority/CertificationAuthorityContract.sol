26be3deebfe72413f77f65bd6a29173756d3ebd8
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2022-10-08 11:32:54 +0200

    Update CertificationAuthority Contract

--- certificationAuthority/CertificationAuthorityContract.sol
+++ certificationAuthority/CertificationAuthorityContract.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.7.1;
 pragma experimental ABIEncoderV2;
 import "../ownable/Ownable.sol";
-import "../libs/Utils.sol";
 import "../tokenManagement/ITokenManagementContract.sol";
 import "./ICertificationAuthorityContract.sol";
 
@@ -20,30 +19,26 @@ contract CertificationAuthorityContract is Ownable, ICertificationAuthorityContr
        tokenManagementAddr = _tokenManagementAddr;
     }
     
-    function addCertificationAuthority(string memory _name, string memory _location, string memory _executiveDirector, uint _defaultCostOfIssuingCertificate) external override CertificationAuthorityMustNotExist(msg.sender) {
-        _addCertificationAuthority(_name,  _location, _executiveDirector, _defaultCostOfIssuingCertificate);
+    function addCertificationAuthority(string memory _id, uint _defaultCostOfIssuingCertificate) external override CertificationAuthorityMustNotExist(msg.sender) {
+        _addCertificationAuthority(_id,  _defaultCostOfIssuingCertificate);
     }
 
-    function addCertificationAuthority(string memory _name, string memory _location, string memory _executiveDirector) external override CertificationAuthorityMustNotExist(msg.sender) {
-        _addCertificationAuthority(_name, _location, _executiveDirector, DEFAULT_COST_OF_ISSUING_CERTIFICATE);
+    function addCertificationAuthority(string memory _id) external override CertificationAuthorityMustNotExist(msg.sender) {
+        _addCertificationAuthority(_id, DEFAULT_COST_OF_ISSUING_CERTIFICATE);
     }
 
-    function _addCertificationAuthority(string memory _name, string memory _location, string memory _executiveDirector, uint _defaultCostOfIssuingCertificate) private {
+    function _addCertificationAuthority(string memory _id, uint _defaultCostOfIssuingCertificate) private {
         uint _senderTokens = ITokenManagementContract(tokenManagementAddr).getTokens(msg.sender);
         require(_senderTokens >= ADD_CERTIFICATION_AUTHORITY_COST_IN_TCS_TOKENS, "You do not have enough tokens to register as Certification Authority");
         require(ITokenManagementContract(tokenManagementAddr).transfer(msg.sender, address(this), ADD_CERTIFICATION_AUTHORITY_COST_IN_TCS_TOKENS), "The transfer could not be made");
-        string memory _caId = Utils.bytes32ToString(keccak256(abi.encodePacked(msg.sender)));
-        certificationAuthorities[msg.sender] = CertificationAuthorityRecord(_caId, _name, _location, _executiveDirector, _defaultCostOfIssuingCertificate, true, true);
+        certificationAuthorities[msg.sender] = CertificationAuthorityRecord(_id, _defaultCostOfIssuingCertificate, true, true);
         authoritiesRegistered.push(msg.sender);
-        emit OnNewCertificationAuthorityCreated(msg.sender, _name, _location, _executiveDirector);
+        emit OnNewCertificationAuthorityCreated(msg.sender, _id);
     }
 
-    function updateCertificationAuthority(string memory _name, string memory _location, string memory _executiveDirector, uint _defaultCostOfIssuingCertificate) external override CertificationAuthorityMustExist(msg.sender) {
-        certificationAuthorities[msg.sender].name = _name;
-        certificationAuthorities[msg.sender].location = _location;
-        certificationAuthorities[msg.sender].executiveDirector = _executiveDirector;
+    function updateCertificationAuthority(uint _defaultCostOfIssuingCertificate) external override CertificationAuthorityMustExist(msg.sender) {
         certificationAuthorities[msg.sender].defaultCostOfIssuingCertificate = _defaultCostOfIssuingCertificate;
-        emit OnCertificationAuthorityUpdated(msg.sender, _name);
+        emit OnCertificationAuthorityUpdated(msg.sender, certificationAuthorities[msg.sender].id);
     }
     
     function removeCertificationAuthority(address _address) external override onlyOwner() CertificationAuthorityMustExist(_address) { 
